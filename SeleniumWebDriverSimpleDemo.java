aaee62c7f0ad561356d7b78f8b57ff313cb6c265
GitHub-chengming <39001896+GitHub-chengming@users.noreply.github.com>
2018-05-11 19:54:44 +0800

    Create SeleniumWebDriverSimpleDemo.java

---
+++ SeleniumWebDriverSimpleDemo.java
@@ -0,0 +1,41 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumWebDriverSimpleDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 如果火狐浏览器没有默认安装在C盘，需要制定其路径
+		// System.setProperty("webdriver.firefox.bin",
+		// "D:/Program Files/Mozilla firefox/firefox.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.baidu.com/");
+
+		driver.manage().window().maximize();
+		WebElement txtbox = driver.findElement(By.name("wd"));
+		txtbox.sendKeys("欧洲杯");
+		WebElement btn = driver.findElement(By.id("su"));
+		btn.click();
+		try {
+			
+			Thread.sleep(3000);
+
+		} catch (InterruptedException e) {
+
+			// TODO Auto-generated catch block
+
+			e.printStackTrace();
+
+		}
+
+		// 为便于查看测试效果，可把关闭浏览器操作注释
+		// driver.close();
+
+	}
+
+}
